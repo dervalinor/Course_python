3dbb365bc0ea1090f84c19fa61a2b03277e3d2b3
dervalinor <dervalar3.14@gmail.com>
2021-10-12 09:33:47 -0500

    Data types

---
+++ 1_types_data/Data_types.py
@@ -0,0 +1,15 @@
+#Data types
+
+#two data type in python
+
+#Text types
+#   1- string(str)
+
+
+#Number types:
+#   1- Integer (int) - whole (entero) number, positive or negative, without decimals
+#   2- Float - decimals, positive or negative
+
+print("Hallo Welt") #string
+print(3.5) #flaot
+print(3) #integer
