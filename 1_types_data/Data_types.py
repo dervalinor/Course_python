ab0f1aac24e18391bd15ceb489ebea8acd562618
dervalinor <dervalar3.14@gmail.com>
2021-10-14 17:25:17 -0500

    main

--- 1_types_data/Data_types.py
+++ 1_types_data/Data_types.py
@@ -13,3 +13,17 @@
 print("Hallo Welt") #string
 print(3.5) #flaot
 print(3) #integer
+
+#Working with numbers - Arithmetic Operators: use for mathematics operations
+
+print(2 + 2) #sum 
+print(2-2) #substraction
+print(2*2) #multiply
+print(2**3) #power
+
+#string concatenation - adding string together as sum of strings
+
+print("Hallo " + "Welt")
+
+#case where we have a string and number then given error this case, we must convert the number in string
+print("Price of action Intel: " + str(52.3) + " dollars")
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Course_python
-Course basic of python
+# Course python
+#Course basic of python, after we will learn Machine Learning, Data Science and Backend
+
+#Themes of course
+#1. work with strings, numbers and booleans
+#2. work with lists, set, dictionaries
+#3. variables and functions
+#4. accepting user input
+#5. input validation with conditionals (if/else)
+#6. error handling with try / except
+#7. looping: for loop, while loop
