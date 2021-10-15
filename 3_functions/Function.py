467e76bb5e1d26a0230a51751890082a6b267d16
dervalinor <dervalar3.14@gmail.com>
2021-10-15 08:12:34 -0500

    Functions and parameters

---
+++ 3_functions/Function.py
@@ -0,0 +1,18 @@
+#function - section of code execute a action
+
+#A function is defined using the def keyword
+
+#create a function for make a multiply of two numbers
+
+number_1= 3
+number_2= 4
+
+def multiply(a, b): #here we give the parameter
+    #parameter can have a name different variables when define the function
+    print(f"multiply of {a} and {b} is: {a*b}")
+
+#now we must call the function for execute the code 
+#Information can be passed into function as parameter
+#Parameter are also called arguments
+
+multiply(number_1, number_2) #give the parameter (name of variables)
