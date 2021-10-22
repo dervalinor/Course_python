8a7875730bf740f9c421287c304bf59f277225f4
dervalinor <dervalar3.14@gmail.com>
2021-10-22 09:29:24 -0500

    Finished Built-In function

--- 11_Built_In_Function/Built_in.py
+++ 11_Built_In_Function/Built_in.py
@@ -7,4 +7,16 @@ Standalone Built-In Functions
 4. int() - Converts value into an integer number
 5. float() - Converts value into an float number
 
+Built-in function on data types
+
+1. Each data type has its own built-in functions
+2. which are useful/makes sense only this specific data type
+
 """
+
+#example:
+
+A = ["Hallo", "Tag"]
+
+A.isdigit() #we aren't apply this function to list to strings
+#because isdigit apply only to numbers no strings
