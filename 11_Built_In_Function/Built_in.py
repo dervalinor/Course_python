3281e8ea4c078c1b7448e3b417d35a75b40c5d0d
dervalinor <dervalar3.14@gmail.com>
2021-10-21 20:58:40 -0500

    Built-in function

---
+++ 11_Built_In_Function/Built_in.py
@@ -0,0 +1,10 @@
+"""
+Standalone Built-In Functions
+
+1. print() - prints to the standard output device 
+2. input() - Asks user for input
+3. set() - Returns a new set
+4. int() - Converts value into an integer number
+5. float() - Converts value into an float number
+
+"""
