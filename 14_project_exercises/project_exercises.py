3df5ae4f726e725f4a1ee1e64ee3098a118c284d
dervalinor <dervalar3.14@gmail.com>
2021-10-24 11:50:27 -0500

    Packages

--- 14_project_exercises/project_exercises.py
+++ 14_project_exercises/project_exercises.py
@@ -2,6 +2,8 @@
 
 Exercise
 
+Time for learning python
+
 we make use of datetime module for the time calculation
 a. Accept user input of goal and a deadline
 b. print back: How much time remains until that deadline
@@ -9,3 +11,4 @@ b. print back: How much time remains until that deadline
 """
 
 import datetime 
+
---
+++ 15_packages/packages.py
@@ -0,0 +1,23 @@
+"""
+
+Modules vs packages
+
+1. Any Python file is a module 
+2. Packages is a collection of python modules
+3. Package must include an _init_.py file
+4. This _init_ file distinguishes a package from a directory
+
+Download packages: pypi.org 
+
+Install packages
+
+pip install name_package
+
+Pip: is a package manager for python, used to install packages
+from the python package index, but also other indexes
+
+"""
+#install package django and import
+import django
+
+#if you want to uninstall a packake: pip uninstall name_package
