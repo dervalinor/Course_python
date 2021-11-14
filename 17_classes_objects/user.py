f1798f60c8adb8331449b25dc99e32590152b0a7
dervalinor <dervalinor@gmail.com>
2021-11-14 17:06:47 -0500

    Create Object of the class user

--- 17_classes_objects/user.py
+++ 17_classes_objects/user.py
@@ -1,7 +1,7 @@
 #login - create a class where have the attributes and methods
 
 #class - is like an object constructor, all classes have a __init__()
-#functio
+#function
 
 #__init__() is executed automatically, whenever (cuando quiera que) we 
 #create the object from this class
@@ -12,15 +12,21 @@ class User:
     #create function __init__
 
     #"self" parameter - is a reference to the current instance of the class
-    def __init__ (self):
+    #is used to access variables that belong to the class 
+    def __init__ (self, email, name, password, current_job_title): #constructor
 
-    email = "nn@nn.com"
-    name = "Sofia N"
-    password = "let"
+        self.email = email
+        self.name = name
+        self.password = password
+        self.current_job_title = current_job_title 
+
     #Methods
-    def change_password():
+    def change_password(self, change_password): #use "self" as parameter for all the methods and create a new variable
         #function for change password
+        self.password = new_password
 
-    def  change_job_title():
+    def  change_job_title(self, new_job):
+        self.current_job_title = new_job
 
-
+#create an object
+user_github = User("melkor@gmail.com", "Melkor", "python3.9", "Programmer Linux")
